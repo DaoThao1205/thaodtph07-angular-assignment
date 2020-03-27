4cec6cba6d8e6cd479b61aceb792bdd73ea1ccc4
DaoThao1205 <daothithao123voc@gmail.com>
2020-03-27 22:18:33 +0700

    4 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,21 +5,19 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
 import { HomeComponent } from './home/home.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-{path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'manager', component: ProductManagerComponent},
-
+  {path: 'add-product', component: ProductAddComponent},
   {path: 'product', component: ProductListComponent},
   {path: 'product/:productID', component: ProductDetailComponent},
   {path: 'product/edit/:productID', component: ProductEditComponent},
   {path: 'product/delete/:productID', component: ProductListComponent},
-
- // {path: 'product/:id', component: ProductDetailComponent},
-
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,13 @@ import { ProductManagerComponent } from './product-manager/product-manager.compo
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductAddComponent } from './product-add/product-add.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
     HttpClientModule, AppRoutingModule],
 
-  declarations: [ AppComponent, HelloComponent, SlideComponent, GalleryComponent, ProductListComponent, ProductDetailComponent, ProductManagerComponent, HomeComponent, ProductEditComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, GalleryComponent, ProductListComponent, ProductDetailComponent, ProductManagerComponent, HomeComponent, ProductEditComponent, ProductAddComponent ],
   
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
