10aad117f3e8cfef99e2f51d0efe652feda6aa79
DaoThao1205 <daothithao123voc@gmail.com>
2020-04-16 21:51:08 +0700

    2 files modified, 51 added, 48 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { HomeComponent } from './home/home.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { AdminProductListComponent } from './admin-product-list/admin-product-list.component';
-import { AdminProductAddComponent } from './admin-product-add/admin-product-add.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactComponent } from './contact/contact.component';
+import { ProductDetailComponent } from './frontend/product-detail/product-detail.component';
+import { ProductListComponent } from './frontend/product-list/product-list.component';
+import { ProductManagerComponent } from './backend/product-manager/product-manager.component';
+import { HomeComponent } from './frontend/home/home.component';
+import { ProductEditComponent } from './backend/product-edit/product-edit.component';
+import { ProductAddComponent } from './backend/product-add/product-add.component';
+import { AdminProductListComponent } from './backend/admin-product-list/admin-product-list.component';
+import { AdminProductAddComponent } from './backend/admin-product-add/admin-product-add.component';
+import { AboutUsComponent } from './frontend/about-us/about-us.component';
+import { ContactComponent } from './frontend/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'contact', component: ContactComponent},
   {path: 'aboutus', component: AboutUsComponent},
-  { path: 'manager', redirectTo: 'manager/product', pathMatch: 'full'},
+  {path: 'manager', redirectTo: 'manager/product', pathMatch: 'full'},
   {path: 'manager/product', component: AdminProductListComponent},
   {path: 'manager/add-product', component: AdminProductAddComponent},
   {path: 'product', component: ProductListComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,31 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { SlideComponent } from './slide/slide.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SlideComponent } from './frontend/slide/slide.component';
+import { GalleryComponent } from './frontend/gallery/gallery.component';
+import { ProductListComponent } from './frontend/product-list/product-list.component';
+import { ProductDetailComponent } from './frontend/product-detail/product-detail.component';
 import { ProductService } from './product.service';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ProductManagerComponent } from './backend/product-manager/product-manager.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AdminSlidebarComponent } from './admin-slidebar/admin-slidebar.component';
-import { AdminTopbarComponent } from './admin-topbar/admin-topbar.component';
-import { AdminProductListComponent } from './admin-product-list/admin-product-list.component';
-import { AdminProductAddComponent } from './admin-product-add/admin-product-add.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './frontend/home/home.component';
+import { ProductEditComponent } from './backend/product-edit/product-edit.component';
+import { ProductAddComponent } from './backend/product-add/product-add.component';
+import { HeaderComponent } from './frontend/header/header.component';
+import { FooterComponent } from './frontend/footer/footer.component';
+import { AdminSlidebarComponent } from './backend/admin-slidebar/admin-slidebar.component';
+import { AdminTopbarComponent } from './backend/admin-topbar/admin-topbar.component';
+import { AdminProductListComponent } from './backend/admin-product-list/admin-product-list.component';
+import { AdminProductAddComponent } from './backend/admin-product-add/admin-product-add.component';
+import { AboutUsComponent } from './frontend/about-us/about-us.component';
+import { ContactComponent } from './frontend/contact/contact.component';
+import { AdminComponent } from './backend/admin/admin.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
     HttpClientModule, AppRoutingModule],
 
-  declarations: [ AppComponent, HelloComponent, SlideComponent, GalleryComponent, ProductListComponent, ProductDetailComponent, ProductManagerComponent, HomeComponent, ProductEditComponent, ProductAddComponent, HeaderComponent, FooterComponent, AdminSlidebarComponent, AdminTopbarComponent, AdminProductListComponent, AdminProductAddComponent, AboutUsComponent, ContactComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, GalleryComponent, ProductListComponent, ProductDetailComponent, ProductManagerComponent, HomeComponent, ProductEditComponent, ProductAddComponent, HeaderComponent, FooterComponent, AdminSlidebarComponent, AdminTopbarComponent, AdminProductListComponent, AdminProductAddComponent, AboutUsComponent, ContactComponent, AdminComponent ],
   
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
